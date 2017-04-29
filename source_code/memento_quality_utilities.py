804f34e05997f8271c497b7d2df6f05af83519df
Shawn M. Jones <jones.shawn.m@gmail.com>
2017-04-28 22:01:09 -0600

    changed memento damage component to use the web service

--- source_code/memento_quality_utilities.py
+++ source_code/memento_quality_utilities.py
@@ -1,18 +1,30 @@
 import os
+import requests
 
-def get_memento_damage(mem_uri):
-    print mem_uri
-    quality_results_stream = os.popen('perl damage_ijdl/measureMemento.pl '+mem_uri)
-    quality_results = quality_results_stream.read()
-    
-    fields = quality_results.split('\n')
-    pct = None
-    total = None
-    for field in fields:
-        if(field.startswith('TOTAL')):
-            total = float(field[6:])
-        elif(field.startswith('PCT_MISSING')):
-            pct = float(field[12:])
+def get_memento_damage(damage_uri, mem_uri):
+    # commenting out Justin's prototype memento damage code, long live his contribution!
+    #quality_results_stream = os.popen('perl damage_ijdl/measureMemento.pl '+mem_uri) 
+    #quality_results = quality_results_stream.read()
+    #
+    #fields = quality_results.split('\n')
+    #pct = None
+    #total = None
+    #for field in fields:
+    #    if(field.startswith('TOTAL')):
+    #        total = float(field[6:])
+    #    elif(field.startswith('PCT_MISSING')):
+    #        pct = float(field[12:])
+
+    #'http://memento-damage.cs.odu.edu/api/damage/http://odu.edu/compsci'
+
+    if damage_uri[-1] == '/':
+        damage_uri = damage_uri[0:-1]
+
+    service_uri = '{}/{}'.format(damage_uri, mem_uri)
+    resp = requests.get(service_uri)
+
+    total = float(resp.json()['total_damage'])
+
     return total
 
 
